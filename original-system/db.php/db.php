a31ffae69fb2131d86580966930136391b38ee09
hisao-matsushita <blackmickey.1303@gmail.com>
2024-09-06 23:21:00 +0900

    9月6日最新

---
+++ original-system/db.php/db.php
@@ -0,0 +1,7 @@
+<?php
+$host = "mysql57.tatsumi-taxi.sakura.ne.jp";
+$dbName = "db.php";
+$user = "tatsumi-taxi";
+$password = "h-03271303";
+$dsn = "mysql:host={$host};dbname={$dbName};charser=utf8";
+?>
